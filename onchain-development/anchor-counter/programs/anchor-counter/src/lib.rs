33f8d3ddd11c400c11d216e97c11bfca03038559
John McLaughlin <georgemac510@gmail.com>
2024-09-08 20:31:28 -0400

    Update anchor-counter

--- onchain-development/anchor-counter/programs/anchor-counter/src/lib.rs
+++ onchain-development/anchor-counter/programs/anchor-counter/src/lib.rs
@@ -1,46 +1,69 @@
 use anchor_lang::prelude::*;
- 
-declare_id!("nEsY41B5WyajSSrNFrnPxySXsXUJtuPJRe1RzHT89Vo");
- 
+
+declare_id!("CGKJFa9LFJWJ7vbkfEkcuyzssMMK77XanLE5QNKThcfQ");
+
+// Size of the anchor discrimitaror, needed for the space calculation.
+const ANCHOR_DISCRIMINATOR: usize = 8;
+
 #[program]
 pub mod anchor_counter {
     use super::*;
- 
+
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // Initialize the counter account with a count of 0.
         let counter = &mut ctx.accounts.counter;
         counter.count = 0;
+
+        // Log the current count.
         msg!("Counter account created. Current count: {}", counter.count);
         Ok(())
     }
- 
+
     pub fn increment(ctx: Context<Update>) -> Result<()> {
+        // Load the current counter value.
         let counter = &mut ctx.accounts.counter;
         msg!("Previous counter: {}", counter.count);
+
+        // Increment the counter value and log it.
         counter.count = counter.count.checked_add(1).unwrap();
         msg!("Counter incremented. Current count: {}", counter.count);
         Ok(())
     }
+
+    pub fn decrement(ctx: Context<Update>) -> Result<()> {
+        // Load the current counter value.
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+
+        // Decrement the counter value and log it.
+        counter.count = counter.count.checked_sub(1).unwrap();
+        msg!("Counter incremented. Current count: {}", counter.count);
+        Ok(())
+    }
 }
- 
+
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
+    #[account(
+        init, 
+        payer = user, 
+        space = ANCHOR_DISCRIMINATOR + Counter::INIT_SPACE,
+    )]
     pub counter: Account<'info, Counter>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
- 
+
 #[derive(Accounts)]
 pub struct Update<'info> {
     #[account(mut)]
     pub counter: Account<'info, Counter>,
     pub user: Signer<'info>,
 }
- 
+
 #[account]
+#[derive(InitSpace)]
 pub struct Counter {
     pub count: u64,
 }
-
-
